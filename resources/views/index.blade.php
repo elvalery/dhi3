8373fec5ca41c57d146b2f0eb900f29623656408
Elena Voloshyna <elvalery@gnolltech.com>
2019-03-19 02:03:14 +0200

    Fix portfolio short names

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -631,7 +631,7 @@
                     @foreach($active_worker->works as $work)
                         @if($work->small_image)
                             <a href="/#works-{{ $work->id }}" class="worker__portfolio_item worker__portfolio_1" style="background-image: url({{ asset('storage/' . $work->small_image )}})">
-                                <p>{{ $work->short_name }}</p>
+                                <p>{{ $work->name }}</p>
                             </a>
                         @endif
                     @endforeach
