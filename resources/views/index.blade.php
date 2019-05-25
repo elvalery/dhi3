de33a2ff7d5a00befa84748e19d015b00acab64e
Elena Voloshina <elvalery@gnolltech.com>
2019-05-25 18:46:02 +0000

    Google tag manager

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,11 +19,20 @@
     <link rel="stylesheet" href="{{ mix('css/slick.css') }}">
     <link rel="stylesheet" href="{{ mix('css/fancy.css') }}">
     <link href="{{ mix('css/all.css') }}" rel="stylesheet" />
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K7J2Z39');</script>
+<!-- End Google Tag Manager -->
+
     <script type="text/javascript" src="//code.jquery.com/jquery-3.1.1.min.js"></script>
     
     <title>@section('title')Design Hub International@show</title>
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K7J2Z39" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <section id="modal_callback" class="container-fluid modal_callback">
     <div class="container">
         <div class="modal_callback_content animated fadeInUp">
