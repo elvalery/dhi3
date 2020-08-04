e852610bb4366665165e6f00c4762883bbec57e1
Elena Voloshyna <elvalery@relicorn.com>
2020-08-04 13:48:07 +0300

    Fix empty phone

---
+++ database/migrations/2020_08_04_103930_fix_contacts_phone.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FixContactsPhone extends Migration {
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up() {
+    Schema::table('contacts', function (Blueprint $table) {
+      $table->string('phone')->nullable()->change();
+    });
+  }
+  
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down() {
+    //
+  }
+}
