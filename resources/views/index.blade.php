cc44323b8e395071dbe9bf6280cbc657dcfa573b
Yurii Salashnyi <yurik.salashnuy@gmail.com>
2020-11-24 19:11:14 +0200

    Disabled fb chat

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -47,29 +47,29 @@
 </head>
 <body>
 <!-- Load Facebook SDK for JavaScript -->
-<div id="fb-root"></div>
-<script>
-  window.fbAsyncInit = function () {
-    FB.init({
-      xfbml: true,
-      version: 'v8.0',
-    })
-  };
+{{--<div id="fb-root"></div>--}}
+{{--<script>--}}
+{{--  window.fbAsyncInit = function () {--}}
+{{--    FB.init({--}}
+{{--      xfbml: true,--}}
+{{--      version: 'v8.0',--}}
+{{--    })--}}
+{{--  };--}}
 
-  (function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0]
-    if (d.getElementById(id)) return
-    js = d.createElement(s)
-    js.id = id
-    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js'
-    fjs.parentNode.insertBefore(js, fjs)
-  }(document, 'script', 'facebook-jssdk'))</script>
+{{--  (function (d, s, id) {--}}
+{{--    var js, fjs = d.getElementsByTagName(s)[0]--}}
+{{--    if (d.getElementById(id)) return--}}
+{{--    js = d.createElement(s)--}}
+{{--    js.id = id--}}
+{{--    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js'--}}
+{{--    fjs.parentNode.insertBefore(js, fjs)--}}
+{{--  }(document, 'script', 'facebook-jssdk'))</script>--}}
 
 <!-- Your Chat Plugin code -->
-<div class="fb-customerchat"
-     attribution=setup_tool
-     page_id="2234950160090811">
-</div>
+{{--<div class="fb-customerchat"--}}
+{{--     attribution=setup_tool--}}
+{{--     page_id="2234950160090811">--}}
+{{--</div>--}}
 <!-- Google Tag Manager (noscript) -->
 <noscript>
   <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TJVB8M8" height="0" width="0"
